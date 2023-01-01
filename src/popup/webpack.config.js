6e6edbf8d225286e0b82f90ad620348c87eb8100
Eric Zhang <46907515+ericzhang11101@users.noreply.github.com>
2023-01-01 13:16:55 -0500

    initial popup

---
+++ src/popup/webpack.config.js
@@ -0,0 +1,35 @@
+
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              compilerOptions: { noEmit: false },
+            },
+          },
+          
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "testtesttest.js",
+    path: path.resolve(__dirname, "..", "..", "build", "popup"),
+  },
+};
