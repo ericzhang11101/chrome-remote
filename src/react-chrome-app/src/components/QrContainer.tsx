228641364b375584c81f4b69222a52a4bff09f33
Eric Zhang <46907515+ericzhang11101@users.noreply.github.com>
2023-01-01 22:56:51 -0500

    fix

---
+++ src/react-chrome-app/src/components/QrContainer.tsx
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react'
+// @ts-ignore
+import QRCode from 'qrcode'
+ 
+
+interface PropTypes {
+    deviceName: string,
+    deviceKey: string
+}
+
+export default function QrContainer({deviceName, deviceKey}: PropTypes) {
+
+    useEffect(() => {
+        const qrUrl = "base-url" + deviceName + "device-key"
+        var canvas = document.getElementById('canvas')
+
+        QRCode.toCanvas(canvas, qrUrl)
+    }, []);
+    return (
+        <div className="qr-container">
+            <canvas id="canvas"></canvas>
+        </div>
+    )
+}
